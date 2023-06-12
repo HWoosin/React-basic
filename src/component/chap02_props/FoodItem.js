a73295bb2f89f98796154ba84e3dcb93fe1dcf95
HWoosin <ws1340cc@naver.com>
2023-06-12 16:25:24 +0900

    이벤트와 스타일에 대해 공부중입니다. 230612

---
+++ src/component/chap02_props/FoodItem.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FoodItem = (props) => {
+    console.log('props: ' + props);
+  return (
+    <li id = 'chinese'>{props.foodName} ({props.price}원)</li>
+  )
+}
+
+export default FoodItem
